418aaeaf4a4a7b5d2ff81b162ffd338cd0106bff
v.zhelanov <syz.vadim@gmail.com>
2025-01-16 18:08:33 +0300

    Создание наброска

---
+++ Home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/style/style.css">
+</head>
+<body>
+    <div class="wrap">
+        <div class="header">
+            <div class="logoslog">
+                <div class="logo">
+                    <img src="https://www.cstom.ru/wp-content/themes/cstom/images/logo.svg">
+                </div>
+                <div class="slogan">
+                    <div class="phone">
+                        <p>8 (4812) 315-115</p>
+                        <p>8 (910) 710-48-24</p>
+                    </div>
+                    <div class="address">
+                        <a href="">г. Смоленск, ул. Кирова, д. 29</a>
+                        <p>по будням с 8:30 до 21:00</p>
+                        <p>в выходные с 9:00 до 18:00</p>
+                    </div>
+                </div>
+            </div>
+            <div class="nav">
+                <a href="">Главная</a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+        </div>
+        <div class="content"></div>
+        <div class="footer"></div>
+    </div>
+</body>
+</html>
