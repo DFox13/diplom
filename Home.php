51487994927f39717214fed9c28618e3b4a46a25
v.zhelanov <syz.vadim@gmail.com>
2025-01-27 19:02:48 +0300

    Изменение в блоке слогана

--- Home.php
+++ Home.php
@@ -14,10 +14,7 @@
                     <img src="https://www.cstom.ru/wp-content/themes/cstom/images/logo.svg">
                 </div>
                 <div class="slogan">
-                    <div class="phone">
-                        <p>8 (4812) 315-115</p>
-                        <p>8 (910) 710-48-24</p>
-                    </div>
+                    
                     <div class="address">
                         <a href="">г. Смоленск, ул. Кирова, д. 29</a>
                         <p>по будням с 8:30 до 21:00</p>
