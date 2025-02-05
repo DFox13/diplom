f5a24ed6c0abbfefbb80e6b05156103237cab4d1
v.zhelanov <syz.vadim@gmail.com>
2025-02-05 19:10:47 +0300

    Вынесен скрипт для Header в отдельный файл

--- template/header.php
+++ template/header.php
@@ -35,57 +35,6 @@
         </div>
         
     
-    <script>
-
-
-        const currentUrl = window.location.href;
-        const url = new URL(currentUrl);
-        const servicesParam = url.pathname.split('/').pop(); 
-
-        if (servicesParam == ""){
-            document.getElementById('valuePage').textContent = 'Главная';
-        }
-        else if (servicesParam == "services"){
-            document.getElementById('valuePage').textContent = 'Услуги';
-        }
-        else if (servicesParam == "about"){
-            document.getElementById('valuePage').textContent = 'О нас';
-        }
-        else if (servicesParam == "stock"){
-            document.getElementById('valuePage').textContent = 'Акции';
-        }
-        else if (servicesParam == "contacts"){
-            document.getElementById('valuePage').textContent = 'Контакты';
-        }
-        else if (servicesParam == "dentists"){
-            document.getElementById('valuePage').textContent = 'Врачи';
-        }
-        else if (servicesParam == "reviews"){
-            document.getElementById('valuePage').textContent = 'Отзывы';
-        }
-
-        console.log(servicesParam);
-
-
-        $(document).ready(function() {
-    
-            $('#menuButton').click(function() {
-                $('#navbar').toggleClass('open');
-            });
-
-            $(window).bind('scroll', function() {
-                var ScrollPos = $(this).scrollTop(), 
-                    HeaderHeight = $('.header').height(); 
-                
-                if (ScrollPos > HeaderHeight) { 
-                    $('.nav').addClass('fixed'); 
-                    $('.content').css('margin-top', '50px');
-                } else { 
-                    $('.nav').removeClass('fixed');
-                    $('.content').css('margin-top', '0'); 
-                }
-            });
-        });
-    </script>
+    <script src="/scripts/scriptHeader.js"></script>
 </body>
 </html>
